24789d8affbaae5c4309ea0126d8b3cc9b55aab8
njonjo <drmnqweno@gmail.com>
2013-04-07 19:30:16 +0200

    added more validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ActiveRecord::Base
   attr_accessible :name, :author_id, :category_id
 
   validates :name, :presence => true
-  
+  validates :author_id, :presence => true
+  validates :category_id, :presence => true  
 
 end
